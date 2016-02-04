23a2cd165d9ef92fb23f389f696f487b455f1dea
laurasher <asherlk1@mac.com>
2016-02-03 18:01:59 -0800

    Back to original font

--- SosoValentines/include/Resources.h
+++ SosoValentines/include/Resources.h
@@ -3,7 +3,7 @@
 
 #define RES_OPEN_SANS		CINDER_RESOURCE( ../resources/, Asap-Regular.ttf, 128, TTF )
 #define RES_KREON_BOLD		CINDER_RESOURCE( ../resources/, Kreon-Bold.ttf, 129, TTF )
-#define LOVELICA						CINDER_RESOURCE( ../resources/, Lovelica.ttf, 128, TTF )
+#define LOVELICA						CINDER_RESOURCE( ../resources/, Lovelica	.ttf, 128, TTF )
 #define HANDWRITTEN						CINDER_RESOURCE( ../resources/, ColorsOfAutumn.ttf, 128, TTF )
-#define BOLD										CINDER_RESOURCE( ../resources/, BebasNeue.otf, 128, TTF )
+#define BOLD										CINDER_RESOURCE( ../resources/, NEOTERIC∏ - Bold DEMO VERSION.ttf, 128, TTF )
 #define CURSIVE										CINDER_RESOURCE( ../resources/, Stackyard_PERSONAL_USE.ttf, 128, TTF )

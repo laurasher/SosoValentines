b3d2f863b03fe660d4ffdcb194e8f7675cdb6a28
laurasher <asherlk1@mac.com>
2016-02-04 09:59:18 -0800

    Made new TextRibbon file, since was sourcing old one from kaleidoscope directory

--- SosoValentines/src/TextRibbon.cpp
+++ SosoValentines/src/TextRibbon.cpp
@@ -11,6 +11,7 @@
 #include "cinder/gl/Batch.h"
 #include "cinder/Text.h"
 #include "cinder/Timeline.h"
+#include "string"
 
 #include "TextRibbon.h"
 #include "Resources.h"
@@ -21,23 +22,47 @@ using namespace std;
 using namespace ci;
 using namespace ci::app;
 
+int textBoxWidth = 750;
+
 TextRibbon::TextRibbon()
-	: mCol(Color::black()), mTextCol(Color::white())
+: mCol(Color::black()), mTextCol(Color::white())
 {
-	mUserFont = Font( loadResource( RES_OPEN_SANS ), 16 );
-	mTagFont = Font( loadResource( RES_KREON_BOLD ), 16 );
+	mUserFont = Font( loadResource( RES_KREON_BOLD ), 30 );
+	//		mUserFont = Font( loadResource( BOLD ), 30 );
+	//	mTagFont = Font( loadResource( CURSIVE ), 40 );
+	mTagFont = Font( loadResource( RES_OPEN_SANS ), 20 );
 }
 
-void TextRibbon::update( string tag, string user )
+void TextRibbon::update( string tag, string user, string mSearchTag )
 {
-	mTag = (tag!="") ? "#" + tag : "";
-	mUser = user;
-	
+	//	mTag = (tag!="") ? "#" + tag : "";
+	//clear previous user text
+	mTrimTag.clear();
+	mTag = "#" + mSearchTag;
+	// trim the fat
+	int i = 0,trimCount=0;
+	while(i<user.length()){
+		if (user[i]=='#'||user[i]=='@'){ //single quotes used for chars, double for strings
+			//jump to space
+			while(user[i]!=' '){
+				i++;
+			}
+		} else{
+			mTrimTag += user[i];
+			i++; trimCount++;
+		}
+	}
+
+	console()<<mTrimTag<<endl;
+	//	mUser = user;
+	mUser = mTrimTag;
+
+
 	mCurPos.value() = vec2(-60, 0);
 	mCurAlpha = 0.0f;
-	
+
 	makeText();
-	
+
 	ribbonIn( 4.0 );
 }
 
@@ -45,7 +70,7 @@ void TextRibbon::ribbonIn( float delay )
 {
 	// animate ribbon in
 	app::timeline().apply( &mCurAlpha, 1.0f, 0.4f, EaseOutQuint()).delay(delay);
-	app::timeline().apply( &mCurPos, vec2( 0, 0 ), 0.4f, EaseOutQuint()).delay(delay);	
+	app::timeline().apply( &mCurPos, vec2( 0, 0 ), 0.4f, EaseOutQuint()).delay(delay);
 }
 
 void TextRibbon::ribbonOut( float delay )
@@ -60,23 +85,25 @@ void TextRibbon::makeText()
 	// reset the textures
 	mTagTex.reset();
 	mUserTex.reset();
-	
+
 	// Create the texture for the text
 	if( ! mTag.empty() ) {
-		mTagBox = TextBox().alignment(TextBox::LEFT).font(mTagFont).size(ivec2(200, TextBox::GROW)).text(mTag);
+
+		mTagBox = TextBox().alignment(TextBox::CENTER).font(mTagFont).size(ivec2( textBoxWidth , TextBox::GROW)).text(mTag);
 		mTagBox.setColor(ColorA(mTextCol.r, mTextCol.g, mTextCol.b, 1));
 		mTagBox.setBackgroundColor(ColorA(0, 0, 0, 0));
 		mTagTex = gl::Texture::create( mTagBox.render() );
 	}
-	
-	mUserBox = TextBox().alignment( TextBox::LEFT ).font( mUserFont ).size( ivec2( 250, TextBox::GROW ) ).text( "@" + mUser );
+
+	mUserBox = TextBox().alignment( TextBox::CENTER ).font( mUserFont ).size( ivec2( textBoxWidth, TextBox::GROW ) ).text( mUser );
+
 	mUserBox.setColor(ColorA(mTextCol.r, mTextCol.g, mTextCol.b, 1));
 	mUserBox.setBackgroundColor( ColorA( 0, 0, 0, 0) );
 	mUserTex = gl::Texture::create( mUserBox.render() );
-	
+
 	float ribbonWidth = mTagBox.measure().x + mUserBox.measure().x + 30;
-	mRibbonSize = vec2(ribbonWidth, 50);
-	mTextPos = vec2(0, getWindowHeight() - mRibbonSize.y - 20);
+	mRibbonSize = vec2(ribbonWidth, 100);
+	mTextPos = vec2(0, getWindowHeight() - mRibbonSize.y - 500);
 }
 
 // Draws the solid shape behind the text
@@ -84,24 +111,24 @@ void TextRibbon::drawTextShape()
 {
 	int boxH = (int)mRibbonSize.y;
 	int boxW = (int)mRibbonSize.x;
-	
+
 	float farPt = sin((M_PI/3)*2) * boxH;
 	float offsetX = mCurPos.value().x;
 	float offsetY = mCurPos.value().y;
-	
+
 	gl::color( mCol.r, mCol.g, mCol.b, mCurAlpha );
 	gl::VertBatch vb( GL_TRIANGLE_STRIP );
-		vb.color( mCol.r, mCol.g, mCol.b, mCurAlpha );
-		vb.vertex( offsetX + 0,				offsetY + 0 );
-		vb.vertex( offsetX + 0,				offsetY + boxH );
-		vb.vertex( offsetX + boxW,			offsetY + 0 );
-		vb.vertex( offsetX + boxW + farPt,	offsetY + boxH );
+	vb.color( mCol.r, mCol.g, mCol.b, mCurAlpha );
+	vb.vertex( offsetX + 0,				offsetY + 0 );
+	vb.vertex( offsetX + 0,				offsetY + boxH );
+	vb.vertex( offsetX + boxW,			offsetY + 0 );
+	vb.vertex( offsetX + boxW + farPt,	offsetY + boxH );
 	vb.draw();
-/*	glEnableClientState( GL_VERTEX_ARRAY );
-	glVertexPointer( 2, GL_FLOAT, 0, &points[0].x );
-	glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
-	glDisableClientState( GL_VERTEX_ARRAY );
-	gl::color( 1, 1, 1, 1 );*/
+	/*	glEnableClientState( GL_VERTEX_ARRAY );
+	 glVertexPointer( 2, GL_FLOAT, 0, &points[0].x );
+	 glDrawArrays( GL_TRIANGLE_STRIP, 0, 4 );
+	 glDisableClientState( GL_VERTEX_ARRAY );
+	 gl::color( 1, 1, 1, 1 );*/
 }
 
 void TextRibbon::draw()
@@ -109,19 +136,21 @@ void TextRibbon::draw()
 	gl::ScopedModelMatrix scopedMat;
 	gl::ScopedColor scopedColor;
 	gl::translate(mTextPos);
-  
-	drawTextShape();
-	
+
+	//	drawTextShape();
+
 	float spacing = 0;
 	gl::color( 1, 1, 1, mCurAlpha );
-	
+
 	// Now draw the text textures:
 	// check it the texture exists and if mTagBox has a height (meaning that there's something in that texture)
 	if( mTagTex && mTagBox.measure().y > 0 ) {
 		spacing = 5;
 		gl::draw( mTagTex, vec2(mCurPos.value().x + TEXT_PADDING_X, (mRibbonSize.y - mTagBox.measure().y)/2) );
+
 	}
-	
+
 	if( mUserTex )
 		gl::draw( mUserTex, vec2(mCurPos.value().x + TEXT_PADDING_X + mTagBox.measure().x + spacing, (mRibbonSize.y - mUserBox.measure().y)/2) );
+	
 }
